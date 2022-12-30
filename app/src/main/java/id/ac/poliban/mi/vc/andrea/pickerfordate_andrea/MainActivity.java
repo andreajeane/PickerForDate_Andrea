bc014b7a33ff9bab906139d2e16d8624d4bf881c
Andrea Jeane Elquita <andreajeane28.aje@gmail.com>
2022-12-30 23:05:18 +0800

    Initial commit

---
+++ app/src/main/java/id/ac/poliban/mi/vc/andrea/pickerfordate_andrea/MainActivity.java
@@ -0,0 +1,49 @@
+package id.ac.poliban.mi.vc.andrea.pickerfordate_andrea;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
+/**
+ * This activity shows a button to trigger the date picker.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * Creates the view.
+     * @param savedInstanceState Saved instance state bundle.
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Handles the button click to create a new date picker fragment and
+     * show it.
+     *
+     * @param view View that was clicked
+     */
+    public void showDatePicker(View view) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(),
+                getString(R.string.datepicker));
+    }
+
+    public void processDatePickerResult(int year, int month, int day) {
+        String month_string = Integer.toString(month + 1);
+        String day_string = Integer.toString(day);
+        String year_string = Integer.toString(year);
+        String dateMessage = (month_string +
+                "/" + day_string +
+                "/" + year_string);
+
+        Toast.makeText(this, getString(R.string.date) + dateMessage,
+                Toast.LENGTH_SHORT).show();
+    }
+
+}
